562f8cf01a8d7a1cb0d311239de881f43eb98425
Harry Jung <harryjung135@gmail.com>
2025-02-07 16:18:54 -0700

    Refined password and error validation.

--- app/index.tsx
+++ app/index.tsx
@@ -1,74 +1,79 @@
-// Date Created: 7/2/25
-//Author: Neeraj Bachwani , Harry Jung
-// Description: This file contains the main login page of the app. It contains two text inputs for the user to enter their username and password. It also contains a button which when pressed calls the checkFields method which in turn calls the checkUsernameLength, checkPasswordFormat and validateCredentials methods. The checkUsernameLength method checks if the username is at least 5 characters long, the checkPasswordFormat method checks if the password is in the correct format and the validateCredentials method checks if the credentials entered by the user match the credentials stored in the credentials.json file. If any of the validations fail, an alert is shown to the user. If all validations pass, the user is routed to the next page (TBD by Abhijith).
-
 import { View, Text, TextInput, Button } from "react-native";
 import { StyleSheet } from "react-native";
 import Validations from '../components/validations';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Credentials from "../credentials.json"
 
 export default function Index() {
-
+  const defaultMsg = 'The following errors were found: \n';
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [alertMsg, setAlertMsg] = useState(defaultMsg);
 
   // press a button to call validation methods on both textInput associated values.
   const checkFields = () => {
-
-
-    checkUsernameLength(username);
+    setAlertMsg(defaultMsg);
+    checkUsernameFormat(username);
     checkPasswordFormat(password);
-
     validateCredentials(username, password);
-    
+
+    if (alertMsg !== defaultMsg) {
+      return;
+    }
   };
 
   // call a validation method from the Validations class which checks if the username is at least 5 characters long.
-  const checkUsernameLength = (username: string) => {
+  const checkUsernameFormat = (username: string) => {
     if (!Validations.validateUsernameLength(username)) {
-      alert('Username must be at least 5 characters long');
+      setAlertMsg(alertMsg => alertMsg + 'Username must be at least 5 characters long. \n');
       return;
     } 
+
+    return true;
   }
 
   //call a validation method from the Validations class which checks if the password is in the correct format.
   const checkPasswordFormat = (password: string) => {
-  
-    if (!Validations.validatePasswordFormat(password)) {
-      alert('Password In wrong format'); 
-      // password too short alert , password must contain at least one uppercase letter, too long should be different ?
+    const validationResult = Validations.validatePasswordFormat(password);
+    // returns bool or array. bit weird.
+    
+    if (Array.isArray(validationResult)) {
+      setAlertMsg(alertMsg => alertMsg + 'Password does not meet requirements. \n');
+      for (let error of validationResult) {
+        setAlertMsg(alertMsg => alertMsg + error + '\n');
+      }
+
       return;
     } 
-    
+    return true;
   };
 
   // validate the credentials entered by the user against the credentials stored in the credentials.json file.
   const validateCredentials = (username: string, password: string) => {
     const user = Credentials.users.find((user) => user.username === username);
 
-
     //checking if user exists
     if (!user){
-      alert("User not found");
+      setAlertMsg(alertMsg => alertMsg + "User not found.");
       return;
     }
 
     //matching the password
     if (user.password !== password){
-      alert("Incorrect password");
+      setAlertMsg(alertMsg => alertMsg + "Incorrect password.");
       return;
     }
 
-    //route to next page if credentials are correct PART - 4 (TBD BY ABHIJITH)
-
+    return true;
   };
+  
   return (
     <View>
       <TextInput style={styles.inputField} placeholder="Username" value={username} onChangeText={setUsername}/>
-      <TextInput style={styles.inputField} placeholder="Password" value={password} onChangeText={setPassword}/>
-      //Add the routing to the button
+      <TextInput secureTextEntry={true} style={styles.inputField} placeholder="Password" value={password} onChangeText={setPassword}/>
+      {/* Add the routing to the button */}
       <Button title="Submit" onPress={() => checkFields()}/>
+      <Text style={styles.errorText}>{alertMsg !== defaultMsg ? alertMsg : ''}</Text>
     </View>
   );
 }
@@ -80,5 +85,8 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 10,
     borderRadius: 5,
+  },
+  errorText: {
+    color: 'red',
   }
 })
--- components/validations.tsx
+++ components/validations.tsx
@@ -3,11 +3,38 @@ class Validations {
         return username.length >= 5;
     }
 
-    static validatePasswordFormat(password: string): boolean {
+    // returns true or an array (which is mutable length). Isn't javascript weird?
+    static validatePasswordFormat(password: string) {
         //define a RegexP. password must be at least 8 characters long. should have at least one uppercase letter, one lowercase letter, one number, and one special character.
-        const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        return re.test(password);
+        let errors = [];
 
+        const min_length = 8;
+        const upper = /[A-Z]/;
+        const lower = /[a-z]/;
+        const special = /[@$!%*?&]/;
+        const number = /\d/;
+
+        if (password.length < min_length) {
+            errors.push('Password must be at least 8 characters long.');
+        }
+
+        if (!upper.test(password)) {
+            errors.push('Password must have at least one uppercase letter.');
+        }
+
+        if (!lower.test(password)) {
+            errors.push('Password must have at least one lowercase letter.');
+        }
+
+        if (!special.test(password)) {
+            errors.push('Password must have at least one special character.');
+        }
+
+        if (!number.test(password)) {
+            errors.push('Password must have at least one number.');
+        }
+        
+        return errors.length === 0 ? true : errors;
     }
 }
 
